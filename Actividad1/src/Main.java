f26106b7131afa5b41582697dee0941e429549b4
Oscar Ist <145413423+OscarIst21@users.noreply.github.com>
2024-03-28 17:48:10 -0700

    Examen unidad 2

---
+++ Actividad1/src/Main.java
@@ -0,0 +1,15 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+public class Main extends JFrame{
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 //Ventana ventana=new Ventana();
+	 //JuegoGato JuegoGato=new JuegoGato();
+	}
+	
+
+}
